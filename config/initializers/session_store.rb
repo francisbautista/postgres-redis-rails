7b781b702dbb0f090440391d685e27373f365bc2
Francis Bautista <francis.bautista07@gmail.com>
2014-10-21 20:58:55 +0800

    Modified for session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
 Rails.application.config.session_store :cookie_store, key: '_pgredis_session'
-# MyApplication::Application.config.session_store :redis_store
+MyApplication::Application.config.session_store :redis_store
